20b0f7e64315c6ccbff8df5e6626ac66c02e25ba
Eduardo Parra <edue17@gmail.com>
2015-10-06 21:10:49 +0200

    Imprime nodos residenciales

--- tarea1.py
+++ tarea1.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
 import sys
-import osmapi
+import lxml
 
+from lxml import etree
 def main():
-    print("hola mundos")
+    doc = etree.parse('ciudadreal.osm')
+    #print(etree.tostring(doc,pretty_print=True ,xml_declaration=True, encoding="utf-8"))
+    raiz=doc.getroot()
+    ways=raiz.findall("way")
+    tipos = ["residential"]
+    for way in ways:
+        if(not way.find('tag') is None):
+            if(way.find('tag').attrib["k"] == "highway"  and way.find('tag').attrib["v"] in tipos):
+                for n in way.findall("nd"):
+                    print(n.attrib["ref"])
+
+
 
 main()
