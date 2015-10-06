4eabcde9c712c0dd0ff5cba2d3cc0285c7595fd2
Eduardo Parra <edue17@gmail.com>
2015-10-07 00:37:58 +0200

    Tarea1

--- tarea1.py
+++ tarea1.py
@@ -10,12 +10,22 @@ def main():
     raiz=doc.getroot()
     ways=raiz.findall("way")
     tipos = ["residential"]
+    id_nodos = [];
     for way in ways:
         if(not way.find('tag') is None):
             if(way.find('tag').attrib["k"] == "highway"  and way.find('tag').attrib["v"] in tipos):
                 for n in way.findall("nd"):
-                    print(n.attrib["ref"])
+                    id_nodos.append(n.attrib["ref"]) #Añade a la lista los nodos de las vias que cumplen los requisitos
 
 
+    id_nodos = list(set(id_nodos)) #Quita los nodos duplicadosgg
+    nodes = raiz.findall("node")
+    info_nodos = {};
+
+    #Crea un diccionario con el id del nodo como key y con la latitud y la longitud de valores
+    for node in nodes:
+        if(node.attrib["id"] in id_nodos):
+            info_nodos[node.attrib["id"]] = [node.attrib["lat"],node.attrib["lon"]]
+    print(info_nodos)
 
 main()
