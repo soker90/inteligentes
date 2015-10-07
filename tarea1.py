9bd22887df80f0ce04cffec62dacb25b8ee5848a
peduajo <eduardomi95@hotmail.com>
2015-10-07 11:53:57 +0200

    cambio de metodo

--- tarea1.py
+++ tarea1.py
@@ -4,13 +4,12 @@ import sys
 import lxml
 
 from lxml import etree
-def main():
+def lectura():
     doc = etree.parse('ciudadreal.osm')
-    #print(etree.tostring(doc,pretty_print=True ,xml_declaration=True, encoding="utf-8"))
     raiz=doc.getroot()
     ways=raiz.findall("way")
     tipos = ["residential","trunk","pedestrian"]
-    id_nodos = [];
+    id_nodos = []
     for way in ways:
         if(not way.find('tag') is None):
             if(way.find('tag').attrib["k"] == "highway"  and way.find('tag').attrib["v"] in tipos):
@@ -20,12 +19,13 @@ def main():
 
     id_nodos = list(set(id_nodos)) #Quita los nodos duplicadosgg
     nodes = raiz.findall("node")
-    info_nodos = {};
+    info_nodos = {}
 
     #Crea un diccionario con el id del nodo como key y con la latitud y la longitud de valores
     for node in nodes:
         if(node.attrib["id"] in id_nodos):
             info_nodos[node.attrib["id"]] = [node.attrib["lat"],node.attrib["lon"]]
-    print(info_nodos)##
+    print(info_nodos)
+    return info_nodos
 
-main()
+tabla_nodos=lectura()
