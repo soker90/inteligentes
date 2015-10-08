8daa7d4a694ef96db061309efa36a4f869499ded
Eduardo Parra <edue17@gmail.com>
2015-10-08 12:45:16 +0200

    Estructura de entrega

---
+++ Carpeta_Fuente/Principal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+import lxml
+
+from lxml import etree
+def lectura():
+    doc = etree.parse('ciudadreal.osm')
+    raiz=doc.getroot()
+    ways=raiz.findall("way")
+    tipos = ["residential","trunk","pedestrian"]
+    id_nodos = []
+    for way in ways:
+        if(not way.find('tag') is None):
+            if(way.find('tag').attrib["k"] == "highway"  and way.find('tag').attrib["v"] in tipos):
+                for n in way.findall("nd"):
+                    id_nodos.append(n.attrib["ref"]) #Añade a la lista los nodos de las vias que cumplen los requisitos
+
+
+    id_nodos = list(set(id_nodos)) #Quita los nodos duplicadosgg
+    nodes = raiz.findall("node")
+    info_nodos = {}
+
+    #Crea un diccionario con el id del nodo como key y con la latitud y la longitud de valores
+    for node in nodes:
+        if(node.attrib["id"] in id_nodos):
+            info_nodos[node.attrib["id"]] = [node.attrib["lat"],node.attrib["lon"]]
+    print(info_nodos)
+    return info_nodos
+
+tabla_nodos=lectura()
