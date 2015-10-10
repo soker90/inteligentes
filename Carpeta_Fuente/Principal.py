6270c8a1b088d2b5687e08737921b689cf43a67b
Eduardo Parra <edue17@gmail.com>
2015-10-10 21:01:27 +0200

    Estructura de la entrega

--- Carpeta_Fuente/Principal.py
+++ Carpeta_Fuente/Principal.py
@@ -1,44 +1,11 @@
 #!/usr/bin/python3
 
-import sys
-import lxml
 import math
 import distancia
+
 import networkx as nx
-
-
 from lxml import etree
-
-def merc_x(lon):
-  r_major=6378137.000
-  return r_major*math.radians(lon)
-
-def merc_y(lat):
-  if lat>89.5:lat=89.5
-  if lat<-89.5:lat=-89.5
-  r_major=6378137.000
-  r_minor=6356752.3142
-  temp=r_minor/r_major
-  eccent=math.sqrt(1-temp**2)
-  phi=math.radians(lat)
-  sinphi=math.sin(phi)
-  con=eccent*sinphi
-  com=eccent/2
-  con=((1.0-con)/(1.0+con))**com
-  ts=math.tan((math.pi/2-phi)/2)/con
-  y=0-r_major*math.log(ts)
-  return y
-
-#Distancia entre dos punto geograficos.
-#Se obtiene sus proyecciones Mercator
-#y la distancia euclidea entre ellas
-def dist(p1lon,p1lat,p2lon,p2lat):
-	x1=merc_x(float(p1lon))
-	x2=merc_x(float(p2lon))
-	y1=merc_y(float(p1lat))
-	y2=merc_y(float(p2lat))
-	return math.sqrt((x1-x2)**2+(y1-y2)**2)
-
+from distancia import *
 
 def lectura():
     doc = etree.parse('ciudadreal.osm')
@@ -83,20 +50,6 @@ def grafo(tabla_nodos,ways):
                 G.add_edge(nodo1,nodo2)
                 G.edge[nodo1][nodo2]['weight']= dist(nodo1Dat[1],nodo1Dat[0],nodo2Dat[1],nodo2Dat[0])
                 i=i+1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 tabla_nodos,ways=lectura()
 grafo(tabla_nodos,ways)
