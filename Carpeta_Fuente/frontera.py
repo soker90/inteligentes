d9bd575b382b4892d54c8908df6455f4836ae552
soker90 <edue17@gmail.com>
2015-10-20 18:03:05 +0200

    Archivo frontera

--- Carpeta_Fuente/frontera.py
+++ Carpeta_Fuente/frontera.py
@@ -1,18 +1,19 @@
-#Implementar una lista ordenada de nodos del  árbol de búsqueda.
-# El orden será de menor a mayor valor de 'Valor' de los nodos y con las siguientes operaciones:
-
-#    CreaFrontera: Crea la frontera vacia y establece el criterio de ordenación.
-#    Insertar(nodoArbol): Añada un nodo nuevo a la frontera.
-#    Elimina(): Devuelve el primer nodo de la frontera y lo elimina de la misma.
-#    EsVacia(): Si o No
 import Queue
 class Frontera():
     def __init__(self):
-        self.lista = None;
+        self.lista = None
 
     def CrearFrontera(self):
         self.lista = Queue.PriorityQueue()
 
-    def Insertar(self,nodoArbol):
-        self.lista.put()
-        Holaaaaa
+    def Insertar(self,nodoArbol,valor):
+        self.lista.put(valor,nodoArbol)
+
+    def Elimina(self):
+        return self.lista.pop(0)
+
+    def EsVacia(self):
+        if(self.lista.qsize() == 0):
+            return 0;
+        else:
+            return 1
