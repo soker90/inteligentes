20d4ebb9d05a5c66e82d5c93c099d6b2f58dc330
peduajo <eduardomi95@hotmail.com>
2015-10-22 17:54:50 +0200

    Problema.py

---
+++ Carpeta_Fuente/Problema.py
@@ -0,0 +1,9 @@
+
+import nodoBusqueda
+
+class Problema():
+
+    def __init__(self,espacioEstados,estadoInicial,estadoObjetivo):
+        self.espacioEstados = espacioEstados
+        self.estadocioInicial = estadoInicial
+        self.estadoObjetivo = estadoObjetivo
